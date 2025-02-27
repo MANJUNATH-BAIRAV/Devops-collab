43b1bb534c14492bf95b56afaa0d8ef8b64db952
MANJUNATH-BAIRAV <manjunathbairav1@gmail.com>
2025-02-27 20:40:29 +0530

    src

---
+++ src/main/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Square of " + num + " is: " + square(num));
+    }
+}
