bc082bc6ae3222d7fbe121e12e67b77bfc80e078
MANJUNATH-BAIRAV <manjunathbairav1@gmail.com>
2025-02-28 13:26:39 +0530

    Update and rename App.java to Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public String greet() {
+        return "Hello, Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().greet());
+    }
+}
