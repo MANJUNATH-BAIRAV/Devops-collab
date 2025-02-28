4834ef1f7de1453b2a07c4e424dd179e1a0f7749
MANJUNATH-BAIRAV <manjunathbairav1@gmail.com>
2025-02-28 17:01:11 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,10 @@
-package com.example;
-
+package com.example; 
 public class Main {
+    public String greet() {
+        System.out.println("Hello, Jenkins!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(new Main().greet());
     }
 }
